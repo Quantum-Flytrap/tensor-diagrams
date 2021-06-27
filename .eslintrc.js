27293995bb1c697ec1d9a5c6abc1d16245549351
Piotr Migdal <pmigdal@gmail.com>
2021-06-27 22:50:09 +0200

    added airbnb TS ESLint style

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
   ],
+  parserOptions: {
+    project: './tsconfig.json',
+  }
 };
