fa982a85930cabaa2869e811d75a74bd0a184956
Piotr Migdal <pmigdal@gmail.com>
2021-08-12 16:23:32 +0200

    refactor - core vs d3js split

--- src/browser.ts
+++ src/browser.ts
@@ -2,7 +2,7 @@
  * This file is the entrypoint of browser builds.
  * The code executes when loaded in a browser.
  */
-import { TensorDiagram } from './diagrams';
+import TensorDiagram from './diagrams-d3js';
 
 // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
